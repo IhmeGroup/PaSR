4f1ef165e9eb222256f682320385c3c92a6e0950
Matthew Bonanni <mbonanni001@gmail.com>
2023-08-24 12:14:29 -0700

    Added more distributions. Settled on inverse Gauss

--- tools/build_hists.py
+++ tools/build_hists.py
@@ -3,6 +3,7 @@ import pandas as pd
 from scipy import integrate
 from scipy import interpolate
 from scipy import stats
+from scipy.special import gamma
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
@@ -26,18 +27,49 @@ plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=XSMALL_SIZE)   # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-mu_arr = 10**(np.linspace(-4, -2, 20))
-sk_arr = np.linspace(0.01, 0.8, 20)
+mu_arr = 10**(np.linspace(-4, -2, 10))
+sk_arr = np.linspace(0.1, 3.0, 10)
+
+# mu_arr = [1.8e-3]
+# # var = 1.0e-7
+# var_arr = 10**np.linspace(-5, -9, 5)
+# var_arr = np.linspace(10**-8, 10**-6, 10)
+# sk = 10.0
+# sk_arr = np.linspace(0.1, 3.0, 5)
 
 data = pd.DataFrame()
 
 for mu in mu_arr:
     for sk in sk_arr:
-        alpha = (2.0 / sk)**2
-        beta = alpha / mu
-        scale = 1 / beta
+    # for var in var_arr:
 
-        mean, var, skew, kurt = stats.gamma.stats(alpha, 0, scale, moments='mvsk')
+        # Gamma distribution
+        # alpha = (2.0 / sk)**2
+        # beta = alpha / mu
+        # scale = 1 / beta
+        # mean, var_test, skew, kurt = stats.gamma.stats(alpha, 0, scale, moments='mvsk')
+
+        # Lognormal distribution
+        # var = np.log((sk**2 + np.sqrt(sk**4 + 4*sk**2) + 2)**(1/3) /
+        #              2**(1/3) + 2**(1/3)/(sk**2 + np.sqrt(sk**4 + 4*sk**2) + 2)**(1/3) - 1)
+        # s = np.sqrt(var)
+        # mu_log = np.log(mu) - var / 2
+        # scale = np.exp(mu_log)
+        # mean, var_test, skew, kurt = stats.lognorm.stats(s, 0, scale, moments='mvsk')
+
+        # Frechet distribution
+        # f = lambda a : ((gamma(1 - 3/a) - 3*gamma(1-2/a)*gamma(1-1/a) + 2*(gamma(1-1/a))**3) /
+        #                 np.sqrt((gamma(1-2/a) - (gamma(1-1/a))**2)**3)) - sk
+        # alpha = fsolve(f, 5)[0]
+        # s = np.sqrt(var / (gamma(1-2/alpha) - (gamma(1-1/alpha))**2))
+        # m = mu - s * gamma(1-1/alpha)
+        # mean, var_test, skew, kurt = stats.invweibull.stats(alpha, m, s, moments='mvsk')
+
+        # Inverse Gauss distribution
+        lam = mu / (sk / 3)**2
+        scale = 1 / lam
+        mean, var, skew, kurt = stats.invgauss.stats(mu/lam, scale=lam, moments='mvsk') 
+
         # print("Mean: {0:.4e}".format(mean))
         # print("Var: {0:.4e}".format(var))
         # print("Skew: {0:.4e}".format(skew))
@@ -45,7 +77,11 @@ for mu in mu_arr:
 
         age = np.linspace(1.0e-5,
                           5 * mu, 1000)
-        pdf = stats.gamma.pdf(age, alpha, 0, scale)
+        
+        # pdf = stats.gamma.pdf(age, alpha, 0, scale)
+        # pdf = stats.lognorm.pdf(age, s, 0, scale)
+        # pdf = stats.invweibull.pdf(age, alpha, m, s)
+        pdf = stats.invgauss.pdf(age, mu/lam, scale=lam)
 
         # pdf[pdf < 1.0e-300] = 0.0 # smallest double for c++
 
@@ -55,8 +91,9 @@ for mu in mu_arr:
         data['hist'] = hist
         data.to_csv("hists/hist_mu_{0:.4e}_var_{1:.4e}_skew_{2:.4e}.csv".format(mean, var, skew), header=False, index=False)
 
-    #     plt.plot(age, pdf, linewidth=2, label=r"$\sigma^2 = {0:.2e}$".format(var))
+    #     plt.plot(age, pdf / np.amax(pdf), linewidth=2, label=r"$\sigma^2 = {0:.2e}$".format(var))
     # plt.xlabel(r"$\tau_{res}$")
     # plt.ylabel(r"$PDF(\tau_{res})$")
     # plt.legend()
+    # plt.savefig("test.png")
     # plt.show()
